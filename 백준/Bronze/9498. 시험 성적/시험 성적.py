23b1384770f957ee74a676780d2086691d805761
scy0502 <138360207+tooooooooooooooooong@users.noreply.github.com>
2025-04-08 21:58:12 +0900

    [Bronze V] Title: 시험 성적, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+input_score = int(input(''))
+
+
+if input_score >= 90:
+    print('A')
+elif input_score >= 80:
+    print('B')
+elif input_score >= 70:
+    print('C')
+elif input_score >= 60:
+    print('D')
+else:
+    print('F')
